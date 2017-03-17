dfc28c62e2a5ef7a33ee5e65260255d36a2804d8
Travis Addair <taddair@uber.com>
2017-03-17 16:39:07 -0700

    Reducing filters back to 128

--- train.py
+++ train.py
@@ -28,7 +28,7 @@ tf.flags.DEFINE_string("embeddings_file", "./glove.6B/glove.6B.100d.txt", "Data 
 # Model Hyperparameters
 tf.flags.DEFINE_integer("embedding_dim", 100, "Dimensionality of character embedding (default: 50)")
 tf.flags.DEFINE_string("filter_sizes", "3,4,5", "Comma-separated filter sizes (default: '3,4,5')")
-tf.flags.DEFINE_integer("num_filters", 256, "Number of filters per filter size (default: 128)")
+tf.flags.DEFINE_integer("num_filters", 128, "Number of filters per filter size (default: 128)")
 tf.flags.DEFINE_float("dropout_keep_prob", 0.5, "Dropout keep probability (default: 0.5)")
 tf.flags.DEFINE_float("l2_reg_lambda", 0.0, "L2 regularization lambda (default: 0.0)")
 
